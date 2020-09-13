50d51c4545ff9d435f04de4ce8972c22afa0081c
mahesh <mahesh.upasani@cloudesign.in>
2020-09-13 16:23:29 +0530

    fix(backend): require change to import

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -1,14 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var app = express();
+// import config from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
+import cookieParser from cookie-parser;
+
+
+// var createError = require('http-errors');
+// var logger = require('morgan');
+const app = express();
 
 // Routes
-var productsRouter = require('./routes/products');
-var usersRouter = require('./routes/users');
+import productRoutes from './routes/productRoutes';
+import userRoutes from './routes/userRoutes';
 
 app.use(cors({
   origin: '*',
@@ -16,33 +20,24 @@ app.use(cors({
   allowedHeaders: 'Content-Type,Authorization'
 }))
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/products', productsRouter);
-app.use('/api/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use('/api/v1/products', productRoutes);
+app.use('/api/v1/users',userRoutes);
+
+app.get("*", (req, res) =>
+  res.status(200).send({
+    message: "Welcome to this API.",
+  })
+);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+export default app;
--- packages/backend/routes/users.js
+++ packages/backend/routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from 'express';
+
+const router = Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', UserController.getAllUsers);
 
 module.exports = router;
